58cbb48dfd12e924e5d02fe403c46ee9440233f7
Rob Webb <rob.webb@loyaltylogistix.com>
2023-12-05 10:20:49 +0000

    Added Temp Timer

---
+++ RobsTempTimer.cs
@@ -0,0 +1,17 @@
+﻿namespace LLP.Reporting.Api.CampaignSummaries;
+
+public static class RobsTempTimer
+{
+    private static DateTime _startTime;
+    private static DateTime _endTime;
+    
+    public static void Start() => _startTime = DateTime.Now;
+    public static void End() => _endTime = DateTime.Now;
+
+    public static double Diff()
+    {
+        var x = _startTime;
+        _startTime = DateTime.Now;
+        return (DateTime.Now - x).TotalSeconds;
+    }
+}
